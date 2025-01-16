c6724345876d9797df084e6dd28e192913c99ce9
KhadijaHameed01 <taibakausar469@gmail.com>
2025-01-16 18:00:29 +0500

    Added changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { FaFacebook, FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
-import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaSquareUpwork } from "react-icons/fa6";
 import { TbBrandFiverr } from "react-icons/tb";
 const Navbar = () => {
@@ -13,19 +12,16 @@ const Navbar = () => {
 
       {/* Social Media Icons */}
       <div className="mt-4 flex items-center justify-center gap-4 text-2xl md:mt-0">
-        <a href="https://www.linkedin.com/in/khadija-hameed-a67320341/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/khadija-hameed-webdeveloper/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
           <FaLinkedin />
         </a>
         <a href="https://github.com/KhadijaHameed01" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
           <FaGithub />
         </a>
-        <a href="https://twitter.com" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
-          <FaSquareXTwitter />
-        </a>
         <a href="https://www.facebook.com/profile.php?id=61571976560471" aria-label="Facebook" target="_blank" rel="noopener noreferrer">
         <FaFacebook />
         </a>
-        <a href="https://www.upwork.com" aria-label="Upwork" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.upwork.com/freelancers/~013da09b047e434cf2" aria-label="Upwork" target="_blank" rel="noopener noreferrer">
         <FaSquareUpwork />
         </a>
         <a href="https://www.fiverr.com" aria-label="Fiverr" target="_blank" rel="noopener noreferrer">
